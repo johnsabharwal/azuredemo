c0396c906cb7095a0da723df1933ab7ea4d9c6cf
john.sabharwal <john.sabharwal@lngconsultancy.co.in>
2021-07-04 11:20:08 +0530

    test comment

--- AzureLearning/Controllers/HomeController.cs
+++ AzureLearning/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace AzureLearning.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        //test
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
